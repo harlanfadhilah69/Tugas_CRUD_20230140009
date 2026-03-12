27d317ffd1840f576f11a1a64fda420eb654a4bc
harlanfadhilah69 <harlancoegg@gmail.com>
2026-03-12 22:28:00 +0700

    finalisasi code

---
+++ praktikumCRUD/src/main/java/com/example/praktikumCRUD/PraktikumCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.praktikumCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraktikumCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraktikumCrudApplication.class, args);
+	}
+
+}
---
+++ praktikumCRUD/src/test/java/com/example/praktikumCRUD/PraktikumCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praktikumCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktikumCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
